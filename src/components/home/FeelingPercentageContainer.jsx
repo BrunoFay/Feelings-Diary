c3e6881ba48211c0143b6f03f30d522a165a9c18
BRUNO <brunofay1@hotmail.com>
2022-05-05 21:43:36 -0300

    FIX css

--- src/components/home/FeelingPercentageContainer.jsx
+++ src/components/home/FeelingPercentageContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import postContext from '../../context/postsContext';
 
 export default function FeelingContainer({ title }) {
