5a78cd3a5a33a982998c63d35299a66a57eae27a
BRUNO <brunofay1@hotmail.com>
2022-06-10 01:33:08 -0300

    NEW implement component by a external library and starts modal logic

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Calendar from 'react-calendar';
 import Navbar from '../components/navbar/Navbar';
 import PageContainer from '../components/Page-container';
 import '../components/schedule/schedule.css';
-
+import 'react-calendar/dist/Calendar.css';
 export default function Schedule() {
-  function countNumberOFdaysInCurrentMonth(year, month) {
-    return new Date(year, month, 0).getDate();
+  const [value, onChange] = useState(new Date());
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function openModal() {
+    console.log("open modal");
+    onChange(value);
+    setIsModalOpen(!isModalOpen);
   }
-  const currentMonth = new Date().getMonth();
-  const currentYear = new Date().getFullYear();
-  const daysOfCurrentMounth = countNumberOFdaysInCurrentMonth(currentYear, currentMonth + 1)
-  const daysArray = Array.from({ length: daysOfCurrentMounth }, (v, i) => i)
-  const nameDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
+  console.log(value);
   return (
     <>
       <Navbar />
       <main className="main-container">
         <PageContainer>
           <h1>Schedule</h1>
-          <div className="schedule-container">
-            <div className='dayName-container'>
-              {nameDays.map((nameDay, index) => (<span >{nameDay}</span>))}
-            </div>
-            <div className='days-container'>
-            {daysArray.map((day, i) => (<div className='day-container'>{i + 1}</div>))}
-            </div>
+          <div className='calendar-container'>
+            <Calendar onChange={openModal} value={value} />
           </div>
+          {isModalOpen && (
+            <div className='modal-container'>
+            <h1>hello</h1>
+            </div>
+          )}
+
         </PageContainer>
       </main>
     </>
