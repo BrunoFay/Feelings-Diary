1ad638ce48749258b635d84915d742244b0f58ed
FAY <brunofay1@hotmail.com>
2022-06-24 01:04:42 -0300

    NEW implement carousel logical on schedulepage

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Calendar from 'react-calendar';
 import Navbar from '../components/navbar/Navbar';
 import PageContainer from '../components/Page-container';
@@ -7,11 +7,14 @@ import 'react-calendar/dist/Calendar.css';
 import NewMarkModal from '../components/schedule/NewMarkModal';
 import { useContext } from 'react';
 import { scheduleContext } from '../context/schedule/scheduleContext';
+import useDragScroll from '../hooks/useDragScroll';
 
 export default function Schedule() {
   const [date, setDate] = useState(new Date());
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { schedule } = useContext(scheduleContext)
+  const carouselRef = useRef(null)
+  const { onMouseDown } = useDragScroll(carouselRef, { direction: 'horizontal' });
   function openModal() {
     setIsModalOpen(true);
   }
@@ -28,15 +31,25 @@ export default function Schedule() {
           <div className='calendar-container'>
             <Calendar onChange={handleDateCalendar} value={date} />
           </div>
-          <div className='schedules-continer'>
-            {schedule.map(({scheduleType,description,scheduleDate})=>(
-            <div>
-            <span>{scheduleType}</span>
-            <span>{description}</span>
-            <span>{scheduleDate}</span>
+          <di>
+            <div className='carousel-container' >
+              <div
+                ref={carouselRef}
+                className='carousel schedules-continer'
+                onMouseDown={onMouseDown}
+                onKeyDown={onMouseDown}
+                tabIndex='0'
+              >
+                {schedule.map(({ scheduleType, description, scheduleDate }) => (
+                  <div className={`carousel-item ${scheduleType}-container`}>
+                    <span>{scheduleType}</span>
+                    <span>{description}</span>
+                    <span>{scheduleDate}</span>
+                  </div>
+                ))}
+              </div>
             </div>
-            ))}
-          </div>
+          </di>
           {isModalOpen && (
             <NewMarkModal date={date} />
           )}
